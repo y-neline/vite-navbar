c9620b37a62349ff88a7b384c73bb257190dfa7c
y_neline <aliya.arapbayeva@gmail.com>
2024-05-31 15:34:52 +0500

    Incomplete Slide in Carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,48 +1,64 @@
-import React, {useEffect, useState} from 'react'
-import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
-import "./Carousel.css"
+import React, { useEffect, useState } from 'react';
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
+import "./Carousel.css";
 
-export const Carousel = ({data}) => {
-    const [slide, setSlide] = useState(0);
+export const Carousel = ({ data }) => {
+  const [slide, setSlide] = useState(0);
+  const [animating, setAnimating] = useState(false);
 
-    const nextSlide = () => {
-        setSlide(slide === data.length - 1 ? 0 : slide + 1);
+  const nextSlide = () => {
+    if (!animating) {
+      setAnimating(true);
+      setSlide((prev) => (prev === data.length - 1 ? 0 : prev + 1));
     }
+  };
 
-    const prevSlide = () => {
-        setSlide(slide === 0 ? data.length - 1 : slide - 1);
+  const prevSlide = () => {
+    if (!animating) {
+      setAnimating(true);
+      setSlide((prev) => (prev === 0 ? data.length - 1 : prev - 1));
     }
+  };
 
-    useEffect(() => {
-        const interval = setInterval(nextSlide, 3000); // Change slide every 3 seconds
-        return () => clearInterval(interval); // Clean up the interval on component unmount
-    }, [slide]);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (!animating) {
+        nextSlide();
+      }
+    }, 3000); // Change slide every 3 seconds
 
-    return (
-        <div className='carousel'>
-            <BsArrowLeftCircleFill className='arrow arrow-left' onClick={prevSlide}/>
-            {data.map((item, idx) => {
-                return (
-                    <img 
-                        src={item.src} 
-                        alt={item.alt} 
-                        key={idx} 
-                        className={`slide ${slide === idx ? "active" : ""}`}
-                    />
-                );
-            })}
-            <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide}/>
-            <div className='indicators'>
-                {data.map((_, idx) => {
-                    return (
-                        <button 
-                            key={idx} 
-                            onClick={() => setSlide(idx)} 
-                            className={`indicator ${slide === idx ? "active" : ""}`}
-                        ></button>
-                    );
-                })}
-            </div>
-        </div>
-    )
-}
+    return () => clearInterval(interval); // Clean up the interval on component unmount
+  }, [animating]);
+
+  return (
+    <div className='carousel'>
+      <BsArrowLeftCircleFill className='arrow arrow-left' onClick={prevSlide} />
+      <div className='slides-wrapper'>
+        {data.map((item, idx) => (
+          <img
+            src={item.src}
+            alt={item.alt}
+            key={idx}
+            className={`slide ${slide === idx ? 'active' : ''} ${animating ? 'animating' : ''}`}
+            onAnimationEnd={() => setAnimating(false)}
+          />
+        ))}
+      </div>
+      <BsArrowRightCircleFill className='arrow arrow-right' onClick={nextSlide} />
+      <div className='indicators'>
+        {data.map((_, idx) => (
+          <button
+            key={idx}
+            onClick={() => {
+              if (!animating) {
+                setAnimating(true);
+                setSlide(idx);
+              }
+            }}
+            className={`indicator ${slide === idx ? 'active' : ''}`}
+          ></button>
+        ))}
+      </div>
+    </div>
+  );
+};
