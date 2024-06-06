f63ae0406ece0fd8225aabeb6066cb053187e74b
y_neline <aliya.arapbayeva@gmail.com>
2024-06-06 11:50:29 +0500

    Added genres

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import Carousel from '../Carousel/Carousel'
-import "./Home.css"
+import React from "react";
+import Carousel from "../Carousel/Carousel";
+import GenreCarousel from "../genres/GenreCarousel";
+import "./Home.css";
 
-import {slides} from "../../data/carouselData.json"
+import { slides } from "../../data/carouselData.json";
 
 export const Home = () => {
-  return (
-    <div className='image-carousel'>
-      <Carousel slides={slides} interval ={3000} />
-    </div>
-  )
-}
+    return (
+        <div className="home">
+            <div className="image-carousel">
+                <Carousel slides={slides} interval={3000} />
+            </div>
+            <div className="genres">
+                <GenreCarousel />
+            </div>
+        </div>
+    );
+};
